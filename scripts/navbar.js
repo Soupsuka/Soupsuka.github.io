96de012140a8907ba33544faeada7f0e14cae5f8
Cecily Cunningham <soupsuka@gmail.com>
2020-02-20 19:59:16 -0600

    working on cgs project problems considerations solutions

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,7 +2,7 @@ function hMenu(x) {
   x.classList.toggle("change");
 }
 
-function dropDown() {
+function dropDown(y) {
   var y = document.getElementById("nav-links");
   if (y.style.display === "block") {
     y.style.display = "none";
